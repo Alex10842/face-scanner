3748c87cde3512265666be44733c38def88f2c1e
Alex10842 <bredfern108@gmail.com>
2019-04-05 09:30:47 +0100

    Updates

---
+++ precache-manifest.d5b574ddd25e0da07a6fb74cdbe816f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1658422215de4f24e01",
+    "url": "/smart-brain/build/static/css/main.c249e525.chunk.css"
+  },
+  {
+    "revision": "d1658422215de4f24e01",
+    "url": "/smart-brain/build/static/js/main.7df2d2c9.chunk.js"
+  },
+  {
+    "revision": "ba472ed1ddb82a931e27",
+    "url": "/smart-brain/build/static/js/runtime~main.c9d908bb.js"
+  },
+  {
+    "revision": "bd8bde5dffa4c210879e",
+    "url": "/smart-brain/build/static/css/2.33b2bb4d.chunk.css"
+  },
+  {
+    "revision": "bd8bde5dffa4c210879e",
+    "url": "/smart-brain/build/static/js/2.59fc4d9f.chunk.js"
+  },
+  {
+    "revision": "7a41d25945872d76353efacebf912389",
+    "url": "/smart-brain/build/index.html"
+  }
+];
